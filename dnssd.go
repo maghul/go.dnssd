ec3d2c9feb111ccb3df2859f92a45670fc845a0b
Magnus Hulthén <magnus@computer.se>
2016-09-17 02:48:01 +0200

    TODOs!

--- dnssd.go
+++ dnssd.go
@@ -43,6 +43,8 @@ func (rrc *rrcache) add(rr dns.RR) {
 
 func (rrc *rrcache) matchQuestion(cmd *command) bool {
 	for _, rr := range rrc.cache {
+		// TODO:Look at TTL and expire things
+		//      Send ServiceUpdate if a PTR record expires.
 		for _, q := range cmd.q.Question {
 			//			fmt.Println("matchQuestion: q=", q, ", rr=", rr)
 			if cmd.match(q, rr) {
@@ -55,6 +57,8 @@ func (rrc *rrcache) matchQuestion(cmd *command) bool {
 
 func (rrc *rrcache) matchAnswers(cmd *command, sections []dns.RR) {
 	for _, rr := range sections {
+		// TODO: Check these RR for TTL=0 and expire RRs
+		//       send a ServiceUpdate with found=false for expired PTR records
 		for _, q := range cmd.q.Question {
 			rrc.add(rr)
 			cmd.match(q, rr)
@@ -82,6 +86,7 @@ func (c *netserver) processing() {
 			//			fmt.Println("COMMAND: ", cmd)
 
 			if !rrc.matchQuestion(cmd) {
+				// TODO: Don't resend queries!
 				fmt.Println("SEND-QUERY-COMMAND: ", cmd)
 				err := c.sendQuery(cmd.q)
 				if err != nil {
