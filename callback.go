64467cc1d3a2320a7534b643e7f96c097c0e300e
Magnus Hulthén <magnus@computer.se>
2016-10-11 17:10:00 +0200

    Threaded callbacks.
    callbacks are called in a separate goroutine to avoid blocking
    the dnssd processing. If the callback blocks a new goroutine
    will be started.

--- callback.go
+++ callback.go
@@ -2,6 +2,7 @@ package dnssd
 
 import (
 	"context"
+	"time"
 )
 
 type callback struct {
@@ -9,8 +10,39 @@ type callback struct {
 	call QueryAnswered
 }
 
+var callbackChan chan func() = make(chan func())
+var callbackThreads int = 0
+
+func callbackThread(num int) {
+	for {
+		call := <-callbackChan
+		dnssdlog("CALL: #", num)
+		call()
+	}
+}
+
 func (r *callback) respond(ifIndex int, a *answer) {
-	r.call(0, ifIndex, a.rr)
+	f := func() {
+		r.call(0, ifIndex, a.rr)
+	}
+
+	dnssdlog("CALLBACK ", r.isValid(), ", ANSWER=", a)
+	select {
+	case callbackChan <- f:
+		return
+	default:
+	}
+
+	t := time.NewTimer(time.Millisecond)
+	select {
+	case callbackChan <- f:
+	case <-t.C:
+		// callback thread isn't reading. start a new one.
+		callbackThreads++
+		go callbackThread(callbackThreads)
+		dnssdlog("------------------> started callback thread #", callbackThreads)
+		callbackChan <- f
+	}
 }
 
 func (cb *callback) isValid() bool {
