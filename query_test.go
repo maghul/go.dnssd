b0626093e44d19ba4befa3c9db0f8687b79c7f15
Magnus Hulthén <magnus@computer.se>
2016-10-15 01:19:02 +0200

    fix query test

--- query_test.go
+++ query_test.go
@@ -9,20 +9,30 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func NoTestQuery1(t *testing.T) {
-	rrc := make(chan dns.RR)
-	ctx := context.Background()
+func TestQuery1(t *testing.T) {
+	ds, _ = makeTestDnssd(t)
+	go ds.processing()
+
+	rrc := make(chan string, 5)
+	defer close(rrc)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	errc := func(err error) {
+		rrc <- fmt.Sprint("TestQuery err=", err)
+	}
+
 	Query(ctx, 0, 0, &dns.Question{Name: "turner.local.", Qtype: dns.TypeA, Qclass: dns.ClassINET},
 		func(flags Flags, ifIndex int, rr dns.RR) {
-			rrc <- rr
-		}, func(err error) {
-			fmt.Println("TestQuery1 err=", err)
-		})
-	assert.NotNil(t, ctx)
-	for ii := 0; ii < 1; ii++ {
-		b := <-rrc
-		fmt.Println("b=", b)
-	}
-	println("done...")
+			dnssdlog("-----------------> ", rr)
+			rrc <- rr.String()
+		}, errc)
+
+	ds.ns.msgCh <- fakeIncomingMsg(true).addRR("turner.local.", dns.TypeA, "10.20.30.40")
+	assert.Equal(t, "turner.local.\t0\tIN\tA\t10.20.30.40", <-rrc)
+
+	assert.Equal(t, 1, len(ds.ns.query.Question))
+	assert.Equal(t, ";turner.local.\tIN\t A", ds.ns.query.Question[0].String())
 
 }
