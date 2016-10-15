457f1daeede10934294cfb67d82e7204fc23c0a4
Magnus Hulthén <magnus@computer.se>
2016-10-15 02:09:24 +0200

    Register tests

--- register_test.go
+++ register_test.go
@@ -4,22 +4,30 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/miekg/dns"
 	"github.com/stretchr/testify/assert"
 )
 
-func NoTestRegister1(t *testing.T) {
-	bc := make(chan string)
+func TestRegister1(t *testing.T) {
+	ds, _ = makeTestDnssd(t)
+	go ds.processing()
+
+	rrc := make(chan string, 5)
+	defer close(rrc)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
 	errc := func(err error) {
-		fmt.Println("TestBrowseAndResolve err=", err)
+		rrc <- fmt.Sprint("TestQuery err=", err)
 	}
+
 	txt := []string{"test=hej", "tjo=hopp"}
-	ctx := context.Background()
 	addrecord := Register(ctx, 0, 3, "Stryfnake", "_tuting._tcp", "", "", 4711, txt, func(flags int, serviceName, regType, domain string) {
 		fmt.Println("Register: serviceName=", serviceName, ", regType=", regType, ",domain=", domain)
-		bc <- fmt.Sprint("Register: serviceName=", serviceName, ", regType=", regType, ",domain=", domain)
+		rrc <- fmt.Sprint("Register: serviceName=", serviceName, ", regType=", regType, ",domain=", domain)
 	}, errc)
 	assert.NotNil(t, ctx)
 
@@ -28,6 +36,30 @@ func NoTestRegister1(t *testing.T) {
 	mxRR.Mx = "xx"
 	addrecord(0, mxRR)
 
-	r := <-bc
-	fmt.Println("RESULT: ", r)
+	assertMessage(t, time.Second, "Register: serviceName=Stryfnake, regType=_tuting._tcp,domain=local", rrc)
+	time.Sleep(1 * time.Millisecond)
+	assert.Equal(t, 3, len(ds.ns.response.Answer))
+	assertResponse(t, "Stryfnake._tuting._tcp.local.\t20\tIN\tSRV\t0 0 4711 flurer.local.", ds.ns.response.Answer)
+	assertResponse(t, "Stryfnake._tuting._tcp.local.\t3200\tIN\tTXT\t\"test=hej\" \"tjo=hopp\"", ds.ns.response.Answer)
+	assertResponse(t, "_tuting._tcp.local.\t3200\tIN\tPTR\tStryfnake._tuting._tcp.local.", ds.ns.response.Answer)
+
+}
+
+func assertMessage(t *testing.T, timeout time.Duration, expected string, msgch <-chan string) {
+	tmr := time.NewTimer(timeout)
+	select {
+	case v := <-msgch:
+		assert.Equal(t, expected, v)
+	case <-tmr.C:
+		assert.Fail(t, fmt.Sprint("Timeout (", timeout.String(), ") waiting for '", expected, "'"))
+	}
+}
+
+func assertResponse(t *testing.T, expected string, rrs []dns.RR) {
+	for _, rr := range rrs {
+		if expected == rr.String() {
+			return
+		}
+	}
+	assert.Fail(t, fmt.Sprint("Could not find response '", expected, "'"))
 }
