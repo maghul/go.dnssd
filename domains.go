9dc71b9e9be9a2845194f130aa7917743558cbc1
Magnus Hulthén <magnus@computer.se>
2016-10-20 12:02:58 +0200

    Implemented domains.
    Will only return "local" as the original implementation.

--- domains.go
+++ domains.go
@@ -4,14 +4,22 @@ import (
 	"context"
 )
 
-// Called to report discovered domains.
-type DomainUpdate func(err error, flags, ifIndex int, domain string)
+/*
+ alled to report discovered domains. Flags can be dnssd.MoreComing and
+dnssd.RecordAdded. The flag dnssd.RecordAdded should be checked to
+determine if a domain was found or lost.
+*/
+type DomainUpdate func(flags Flags, ifIndex int, domain string)
 
-// Asynchronously enumerate domains available for browsing and registration.
-func EnumerateDomains(ctx context.Context, flags, ifIndex int, listener DomainUpdate) {
-	panic("NYI")
+/*
+Asynchronously enumerate domains available for browsing and registration. Flags
+can be dnssd.BrowseDomains or dnssd.RegistrationDomains. ifIndex is ignored
+currently but may be used in the future if there are interface specific domains.
+*/
+func EnumerateDomains(ctx context.Context, flags Flags, ifIndex int, listener DomainUpdate, errc ErrCallback) {
+	listener(RecordAdded, ifIndex, getOwnDomainname())
 }
 
 func getOwnDomainname() string {
-	return "local" // TODO: fix?
+	return "local"
 }
